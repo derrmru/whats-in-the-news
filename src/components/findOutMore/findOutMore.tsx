b0671b57b1fdda4cc8810b230b4426f71c43d11b
derrmru <podogomacbookair@MacBook-Air.local>
2020-10-25 15:53:09 +0100

    topic graph styling

--- src/components/findOutMore/findOutMore.tsx
+++ src/components/findOutMore/findOutMore.tsx
@@ -16,10 +16,10 @@ const FindOutMore: React.FC = () => {
                         <div className="exit-button">|x|</div>
                         <h2>About</h2>
                         <p>Is the News bad for you? What kinds of ideas am I exposed to in the News? Is the News biased? Is the News too negative?</p>
-                        <p>The above is a set of statistical tools to help answer these kinds of questions. This is a Proof Of Concept and requires considerably more data to provide any kind of comprehensive answer to these questions.</p>
+                        <p>The above is a set of statistical tools to help answer these kinds of questions. This is a Proof Of Concept and requires considerably more data to provide any comprehensive answer to these questions.</p>
                         <h3>About the data</h3>
                         <p>The data sources are four of the biggest News organisations. Textual data is scraped from these sources and visualised into meaningful categorisations.</p>
-                        <p>Sentiment analysis categories are identified by crosschecking word lists written by others. These are not moderated or curated for this project.</p>
+                        <p>Sentiment analysis categories are identified by crosschecking word lists written by others. These are made by third parties and are not moderated or curated for or by this project.</p>
                     </div> : 
                         <div style={{textDecoration: 'underline'}}>Find Out More</div>
             }
